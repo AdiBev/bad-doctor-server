2ad3a10067bd3227cc9a6a5b24d1e79a27ae5571
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-07 22:13:02 +0100

    Add api key from the server

--- src/graphql-client.js
+++ src/graphql-client.js
@@ -2,13 +2,11 @@ const client = require("graphql-request");
 const { GraphQLClient } = client;
 
 const endpoint = "https://api.yelp.com/v3/graphql";
-const apiToken =
-  "rwHmIJ9ipaXySeJ_FpnX9dNMRev40UvFDiGCbq951Vl2lY1Me4ZJU5nWD5peGUxADYzpI0OEes12ZJkWS4hngZhutf45XSXXk2HxF6bwQ9KeUOFKmOCrUapyF4vbXnYx";
 
 const graphQLCLient = new GraphQLClient(endpoint, {
   headers: {
     "Access-Control-Allow-Origin": "*",
-    Authorization: `Bearer ${apiToken}`,
+    Authorization: `Bearer ${process.env.YELP_API_KEY}`,
   },
 });
 
