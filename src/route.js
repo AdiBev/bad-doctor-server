1c117b8c1841c08c8ac837689d231c24f52046a5
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-07 21:33:50 +0100

    Add route handler to get graphql query data

---
+++ src/route.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const graphQLCLient = require("./graphql-client");
+const searchQuery = require("./query");
+
+router.get("/", async (req, res) => {
+  const { limit, term, location } = req.query;
+  const variables = {
+    term,
+    location,
+    limit: parseInt(limit),
+  };
+  try {
+    const data = await graphQLCLient.request(searchQuery, variables);
+    res.send(JSON.stringify(data));
+  } catch (err) {
+    res.status(500).send("Error", err.message);
+  }
+});
+
+module.exports = router;
