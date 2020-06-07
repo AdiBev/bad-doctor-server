56ed7c441a0cb46d45b66c868a4239d9603338a0
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-07 21:33:00 +0100

    Add yelp graphql search query

---
+++ src/query.js
@@ -0,0 +1,12 @@
+const searchQuery = `
+    query searchPlaces($term: String, $location: String, $limit: Int) {
+        search(term: $term, location: $location, limit: $limit) {
+          total
+          business {
+            name
+          }
+        }
+    }
+      `;
+
+module.exports = searchQuery;
