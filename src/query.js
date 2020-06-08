a4f871fc4f17365190b10f7bf948858bef54fbaf
AdiBevoor <58564548+AdiBevoor@users.noreply.github.com>
2020-06-08 22:07:34 +0100

    Add more fields to the searchPlaces query

--- src/query.js
+++ src/query.js
@@ -1,10 +1,30 @@
 //Graphql Query file
 const searchQuery = `
-    query searchPlaces($term: String, $location: String, $limit: Int) {
-        search(term: $term, location: $location, limit: $limit) {
+    query searchPlaces($term: String, $location: String, $limit: Int, $offset: Int) {
+        search(term: $term, location: $location, limit: $limit, offset: $offset) {
           total
           business {
             name
+            phone
+            id
+            price
+            location {
+              address1
+              city
+              postal_code
+              country
+            }
+            rating
+            review_count
+            url
+            photos
+            reviews {
+              rating
+              user {
+                name
+              }
+              text
+            }
           }
         }
     }
